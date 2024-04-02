d7d78e9a7ed17364c5178fdc0a4bf2deb0c99d95
konsolebox <konsolebox@gmail.com>
2024-04-02 15:18:31 +0800

    Add helpers/update-profiles-mask.bash

---
+++ helpers/update-profiles-mask.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -p
+
+set +o posix +m -o pipefail || exit 1
+shopt -s lastpipe || exit 1
+
+function die {
+	printf '%s\n' "$1" >&2
+	exit "${2-1}"
+}
+
+function main {
+	local pkgcheck=(pkgcheck scan --exit=error) profiles
+	local desc_file=/var/db/repos/gentoo/profiles/profiles.desc
+
+	# Extract new profiles
+	awk '{ $0 = "" $2 } /^default\// && /\/musl(\/|$)/' "${desc_file}" | \
+			readarray -t profiles || die "Failed to enumerate profile names"
+	[[ ${profiles+.} ]] || die "No musl-based profile names found"
+
+	# Add old profiles
+	readarray -tO "${#profiles[@]}" profiles < profiles.mask || die "Failed to add old profiles"
+
+	# Save
+	printf '%s\n' "${profiles[@]}" | sort -u > profiles.mask || die "Failed to save profiles"
+}
+
+main "$@"
