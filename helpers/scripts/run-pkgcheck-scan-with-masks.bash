5605a26708f124eed74eb9009ec4d40a90c04c3f
konsolebox <konsolebox@gmail.com>
2026-05-17 13:31:08 +0800

    helpers/scripts/run-pkgcheck-scan-with-masks.bash: Forward arguments

--- helpers/scripts/run-pkgcheck-scan-with-masks.bash
+++ helpers/scripts/run-pkgcheck-scan-with-masks.bash
@@ -4,4 +4,4 @@ set -e +o posix +o nounset || exit
 root=${BASH_SOURCE%/*}/../..
 readarray -t profiles < "${root}/helpers/profiles.mask"
 [[ -e ${root}/profiles/package.mask ]] && echo "WARNING: profiles/package.mask exists"
-IFS=, eval 'exec pkgcheck scan --exit=error "--profiles=${profiles[*]/#/-}"'
+IFS=, eval 'exec pkgcheck scan --exit=error "--profiles=${profiles[*]/#/-}" "$@"'
